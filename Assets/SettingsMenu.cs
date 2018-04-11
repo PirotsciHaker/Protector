c28a63152b01d32a6fbc4ec9e2b98e5b927fd307
Duusaan <drplazma@live.com>
2018-04-11 14:37:50 +0200

    Meni i zvuk

---
+++ Assets/SettingsMenu.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class SettingsMenu : MonoBehaviour {
+
+    public AudioMixer audiomixer;
+
+    public void SetVolume(float volume)
+    {
+        audiomixer.SetFloat("volume", volume);
+        
+    }
+
+    public AudioMixer audiomixer2;
+    public void Music(float volume)
+    {
+        audiomixer2.SetFloat("music", volume);
+    }
+}
