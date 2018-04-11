eecb0a9c56b4a30bb789f18260dd28e67643ba79
Duusaan <drplazma@live.com>
2018-04-11 21:38:39 +0200

    dugme za podesavanja

---
+++ Assets/Resume.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Resume : MonoBehaviour {
+
+    public GameObject Pause;
+    public void NejePauza()
+    {
+      Pause.gameObject.SetActive(false);
+    }
+
+
+	
+}
+
---
+++ Assets/settingbutton.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class settingbutton : MonoBehaviour {
+
+    public GameObject SettingMenu;
+    
+    public void Canvas()
+    {
+        SettingMenu.gameObject.SetActive(true);
+    }
+    
+
+    }
+
+
+
+
+
